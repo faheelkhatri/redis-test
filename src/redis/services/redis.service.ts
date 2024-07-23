2c743e6e3cb1eeab014286e06b4a16f07a46f9f0
Deacart <faheel@dealcart.io>
2024-07-23 12:41:29 +0500

    add logging

--- src/redis/services/redis.service.ts
+++ src/redis/services/redis.service.ts
@@ -1,9 +1,33 @@
-import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
 import { Cache } from 'cache-manager';
 
 @Injectable()
-export class RedisService {
+export class RedisService implements OnModuleInit {
+  private readonly logger = new Logger(RedisService.name);
+
   constructor(@Inject(CACHE_MANAGER) private cacheService: Cache) {}
+
+  async onModuleInit() {
+    try {
+      // Attempt a connection to the Redis server by performing a simple operation
+      await this.cacheService.set('connectionTest', 'success');
+      const result = await this.cacheService.get('connectionTest');
+
+      if (result === 'success') {
+        this.logger.log('Redis connection successful');
+      } else {
+        this.logger.error('Redis connection test failed');
+      }
+    } catch (error) {
+      this.logger.error('Redis connection unsuccessful', error.message);
+    }
+  }
 
   async getCache<T>(key: string): Promise<T> {
     return this.cacheService.get(key);
